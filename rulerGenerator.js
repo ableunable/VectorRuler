6199856ac5c6d14e942a1d2b191f32d47d1c8985
Stefan Šućur <stefansucur0@gmail.com>
2023-01-23 18:20:51 +0100

    Formatting for readability

--- rulerGenerator.js
+++ rulerGenerator.js
@@ -2,43 +2,43 @@
 var ruler = {}
 
 
-var limitTickQty = function(){
+var limitTickQty = function() {
     //Prevent it from crashing if it tries to render too many linest
-    ruler.ticksPerUnit = Math.pow(ruler.subUnitBase,ruler.subUnitExponent)
+    ruler.ticksPerUnit = Math.pow(ruler.subUnitBase, ruler.subUnitExponent)
     ruler.masterTickQty = ruler.ticksPerUnit * ruler.width
-    if(ruler.height>100){
+    if(ruler.height>100) {
         console.info("Unreasonable ruler height: "+ ruler.height + "; reducing height.")
-         ruler.height= 15
-         document.getElementById("rulerHeight").value = ruler.height;
+        ruler.height= 15
+        document.getElementById("rulerHeight").value = ruler.height;
     }
-    if(ruler.width>1000){
+    if(ruler.width>1000) {
         console.info("Unreasonable tick quantity: "+ ruler.masterTickQty + "; reducing width.")
-         ruler.width= 500
-         document.getElementById("rulerWidth").value = ruler.width;
+        ruler.width= 500
+        document.getElementById("rulerWidth").value = ruler.width;
     }
-     if(ruler.masterTickQty > 10000){
+     if(ruler.masterTickQty > 10000) {
         console.info("Unreasonable tick quantity: "+ ruler.masterTickQty + "; reducing exponent.")
-        if(ruler.subUnitExponent>1){
-        ruler.subUnitExponent = ruler.subUnitExponent -1
-        document.getElementById("subUnitExponent")[ruler.subUnitExponent].selected = true;
+        if(ruler.subUnitExponent>1) {
+            ruler.subUnitExponent = ruler.subUnitExponent -1
+            document.getElementById("subUnitExponent")[ruler.subUnitExponent].selected = true;
         }
      }
-    if(ruler.ticksPerUnit > 100){
+    if(ruler.ticksPerUnit > 100) {
         console.info("Unreasonable exponent: "+ ruler.ticksPerUnit+ "; resetting to reasonable.")
         ruler.subUnitExponent = 1
         document.getElementById("subUnitExponent")[ruler.subUnitExponent].selected = true;//selects resonable
     }
 }
 
-var checkUnit = function(){
+var checkUnit = function() {
     var pixelsPerInch = 72//I don't think this needs to be in the object....
     var pixelsPerCM  =  pixelsPerInch / ruler.cmPerInch
 
-    if (ruler.units === "inches"){
+    if (ruler.units === "inches") {
         ruler.pixelsPerUnit = pixelsPerInch
         ruler.unitsAbbr= "\"in."
     }
-    else if (ruler.units === "centimeters"){
+    else if (ruler.units === "centimeters") {
         ruler.unitsAbbr= "cm."
         ruler.pixelsPerUnit = pixelsPerCM
     }
@@ -49,33 +49,34 @@ var checkUnit = function(){
     ruler.heightPixels = ruler.height * ruler.pixelsPerUnit
 }
 
-var checkSubUnitBase = function(){
+var checkSubUnitBase = function() {
     // if it is fractional, make the fractional dropdown appear
     // if it is decimal, likewise
     var suffix = " " + ruler.unitsAbbr
 
     var subLabelsDec = [
-   "1"  +suffix,
-   "1/10th"  +suffix,
-   "1/100th"  +suffix,
-   "1/1000th"  +suffix,
-   "1/10000th"  +suffix,
-   "1/100000th"  +suffix,
-   "1/1000000th"  +suffix,
+        "1"  +suffix,
+        "1/10th"  +suffix,
+        "1/100th"  +suffix,
+        "1/1000th"  +suffix,
+        "1/10000th"  +suffix,
+        "1/100000th"  +suffix,
+        "1/1000000th"  +suffix,
     ]
 
     var subLabelsFrac = [
-   "1"  +suffix,
-   "1/2"  +suffix,
-   "1/4"  +suffix,
-   "1/8"  +suffix,
-   "1/16"  +suffix,
-   "1/32"  +suffix,
-   "1/64"  +suffix,
+       "1"  +suffix,
+       "1/2"  +suffix,
+       "1/4"  +suffix,
+       "1/8"  +suffix,
+       "1/16"  +suffix,
+       "1/32"  +suffix,
+       "1/64"  +suffix,
     ]
 
-    if (ruler.subUnitBase === '10'){//Decimal!
-    ruler.subLabels = subLabelsDec
+    if (ruler.subUnitBase === '10') {//Decimal!
+        ruler.subLabels = subLabelsDec
+
         document.getElementById("subUnitExponent")[3].disabled = true;
         document.getElementById("subUnitExponent")[4].disabled = true;//disable the ones that crash.
         document.getElementById("subUnitExponent")[5].disabled = true;
@@ -84,8 +85,7 @@ var checkSubUnitBase = function(){
         for (var i = ruler.subLabels.length - 1; i >= 0; i--) {
             document.getElementById("subUnitExponent")[i].text =ruler.subLabels[i]
         }
-    }
-    else if (ruler.subUnitBase === '2'){//Fractional!
+    } else if (ruler.subUnitBase === '2') {//Fractional!
         ruler.subLabels = subLabelsFrac
 
         document.getElementById("subUnitExponent")[3].disabled = false;
@@ -93,54 +93,55 @@ var checkSubUnitBase = function(){
         document.getElementById("subUnitExponent")[5].disabled = false;//re-enable the ones that dont crash
         document.getElementById("subUnitExponent")[6].disabled = false;
 
-            for (var j = ruler.subLabels.length - 1; j >= 0; j--) {
-                document.getElementById("subUnitExponent")[j].text =ruler.subLabels[j]
-            }
-    }
-    else{
+        for (var j = ruler.subLabels.length - 1; j >= 0; j--) {
+            document.getElementById("subUnitExponent")[j].text =ruler.subLabels[j]
+        }
+    } else{
         console.error("Impossible subUnitBase. Must be 2 or 10. is:  "+ ruler.subUnitBase)
     }
 }
 
-var resizeCanvas = function(){
-    document.getElementById("myCanvas").width = ruler.width*ruler.pixelsPerUnit;
+var resizeCanvas = function() {
+    document.getElementById("myCanvas").width = ruler.width * ruler.pixelsPerUnit;
     heightAddend = 50
-    document.getElementById("myCanvas").height = heightAddend+ ruler.height*ruler.pixelsPerUnit;
+    document.getElementById("myCanvas").height = heightAddend + ruler.height*ruler.pixelsPerUnit;
 }
 
-var constructRuler = function(){
+var constructRuler = function() {
     ruler.tickArray = [];//for prevention of redunancy, an member for each tick
     var layerArray = new Array(ruler.subUnitExponent)//Layers in the SVG file.
 
     for (var exponentIndex = 0;  exponentIndex <= ruler.subUnitExponent ;  exponentIndex++) {
         //loop thru each desired level of ticks, inches, halves, quarters, etc....
-        var tickQty = ruler.width * Math.pow(ruler.subUnitBase,exponentIndex)
+        var tickQty = ruler.width * Math.pow(ruler.subUnitBase, exponentIndex)
         layerArray[exponentIndex]= new paper.Layer();
         layerArray[exponentIndex].name = ruler.subLabels[exponentIndex] + " Tick Group";
 
         var startNo = $('#startNo').val() ;
 
-        highestTickDenomonatorMultiplier = ruler.ticksPerUnit / Math.pow(ruler.subUnitBase,exponentIndex)
+        highestTickDenomonatorMultiplier = ruler.ticksPerUnit / Math.pow(ruler.subUnitBase, exponentIndex)
         //to prevent reduntant ticks, this multiplier is applied to crrent units to ensure consistent indexing of ticks.
         for (var tickIndex = 0;  tickIndex <= tickQty ;  tickIndex++) {
             ruler.masterTickIndex = highestTickDenomonatorMultiplier * tickIndex
             // levelToLevelMultiplier =0.7
             var tickHeight
-            tickHeight = ruler.heightPixels*Math.pow(ruler.levelToLevelMultiplier,exponentIndex)
+            tickHeight = ruler.heightPixels*Math.pow(ruler.levelToLevelMultiplier, exponentIndex)
 
-            var tickSpacing = ruler.pixelsPerUnit/(Math.pow(ruler.subUnitBase,exponentIndex))
+            var tickSpacing = ruler.pixelsPerUnit/(Math.pow(ruler.subUnitBase, exponentIndex))
             //spacing between ticks, the fundemental datum on a ruler :-)
             var finalTick = false
-            if(tickIndex === tickQty){finalTick = true}
+            if(tickIndex === tickQty) {
+                finalTick = true
+            }
 
             var offsetTickIndex = parseInt(tickIndex) + parseInt(startNo)
-            tick(tickHeight,0, tickIndex, offsetTickIndex, exponentIndex, tickSpacing,finalTick);
+            tick(tickHeight, 0, tickIndex, offsetTickIndex, exponentIndex, tickSpacing, finalTick);
             //draws the ticks
         }
     }
 }
 
-var tick = function(tickHeight, horizPosition, tickIndex, offsetTickIndex, exponentIndex, tickSpacing,finalTick){
+var tick = function(tickHeight, horizPosition, tickIndex, offsetTickIndex, exponentIndex, tickSpacing, finalTick) {
     //exponentIndex is 0-6, how small it is, 6 being smallest
     var x1 = horizPosition + (tickSpacing * tickIndex)
     var x2 = x1 //x === x because lines are vertical
@@ -156,40 +157,48 @@ var tick = function(tickHeight, horizPosition, tickIndex, offsetTickIndex, expon
 
         ruler.tickArray[ruler.masterTickIndex]=true //register the tick so it is not duplicated
             if (exponentIndex === 0) {//if is a primary tick, it needs a label
-                tickLabel(x1,y2,finalTick,offsetTickIndex,exponentIndex)
+                tickLabel(x1, y2, finalTick, offsetTickIndex, exponentIndex)
             }
     }   
 }
 
-var tickLabel = function(x1,y2,finalTick,tickIndex,exponentIndex){
+var tickLabel = function(x1, y2, finalTick, tickIndex, exponentIndex) {
     //label the tick
-            var labelTextSize
-            var labelTextSizeInches = 18
-            var labelTextSizeCm = Math.round(labelTextSizeInches/ruler.cmPerInch)
-            if(ruler.units === "inches"){labelTextSize = labelTextSizeInches;}
-            else{labelTextSize = labelTextSizeCm;}
-            var xLabelOffset = 4
-            var yLabelOffset = 1
-            if (finalTick) {xLabelOffset = -1* xLabelOffset}//last label is right justified
-            var text = new paper.PointText(new paper.Point(x1+ xLabelOffset, y2+yLabelOffset));
-            text.justification = 'left';
-            if (finalTick) {text.justification = 'right';}//last label is right justified
-            text.fillColor = 'black';
-            text.content = tickIndex;
-            text.style = {
-            // fontFamily: 'Helvetica',
-            fontFamily: 'monospace',
-            fontWeight: 'bold',
-            fontSize: labelTextSize}
-            text.name = ruler.subLabels[exponentIndex] + " label no. " +tickIndex //label for SVG editor
+    var labelTextSize
+    var labelTextSizeInches = 18
+    var labelTextSizeCm = Math.round(labelTextSizeInches/ruler.cmPerInch*1.5)//cm text was a bit too small
+    if (ruler.units === "inches") {
+        labelTextSize = labelTextSizeInches;
+    } else {
+        labelTextSize = labelTextSizeCm;
+    }
+    var xLabelOffset = 4
+    var yLabelOffset = 1
+    if (finalTick) {
+        xLabelOffset = -1* xLabelOffset
+    }//last label is right justified
+    var text = new paper.PointText(new paper.Point(x1+ xLabelOffset, y2+yLabelOffset));
+    text.justification = 'left';
+    if (finalTick) {
+        text.justification = 'right';
+    }//last label is right justified
+    text.fillColor = 'black';
+    text.content = tickIndex;
+    text.style = {
+        // fontFamily: 'Helvetica',
+        fontFamily: 'monospace',
+        fontWeight: 'bold',
+        fontSize: labelTextSize
+    }
+    text.name = ruler.subLabels[exponentIndex] + " label no. " +tickIndex //label for SVG editor
 }
 
-var debug = function(){
+var debug = function() {
     console.info("--All the variables---")
     console.info(ruler)//prints all attributes of ruler object
 }
 
-var updateVariables = function(){
+var updateVariables = function() {
     ruler.units =  $("input:radio[name=rulerUnits]:checked'").val();
     ruler.subUnitBase = $("input:radio[name=subUnits]:checked'").val();
     ruler.redundant =  $("input:checkbox[name=redundant]:checked'").val();
@@ -200,7 +209,7 @@ var updateVariables = function(){
     ruler.cmPerInch = 2.54
 }
 
-var build = function(){
+var build = function() {
     // Get a reference to the canvas object
     var canvas = document.getElementById('myCanvas');
     // Create an empty project and a view for the canvas:
@@ -217,10 +226,10 @@ var build = function(){
     paper.view.draw();
 }
 
-var exportSvg = function(){
+var exportSvg = function() {
     //* I referenced the excellent SVG export example here: http://paperjs.org/features/#svg-import-and-export
     document.getElementById("svgexpbutton").onclick = 
-    function(){
+    function() {
         exportWidth = document.getElementById("myCanvas").width
         exportHeight = document.getElementById("myCanvas").height
 
@@ -248,7 +257,7 @@ var exportSvg = function(){
 
 }
 
-$(document).ready(function(){ 
+$(document).ready(function() { 
     console.log("\t Welcome to the Ruler Generator │╵│╵│╵│╵│╵│╵│")
     //When document is loaded, call build once
     build()
